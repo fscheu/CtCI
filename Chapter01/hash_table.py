59461be69d29007c241d230103a0e029d45b2905
Federico Scheu <fscheu@gmail.com>
2022-07-22 21:51:47 -0300

    Hash Table first version with pytests

---
+++ Chapter01/hash_table.py
@@ -0,0 +1,56 @@
+#   HashTable implementation
+#   with an array of LinkedList
+#
+#
+
+class LinkedList:
+    def __init__(self, val=None, next=None) -> None:
+        self.val=val
+        self.next = next
+
+    def add(self, val):
+        self = LinkedList(val,self.next)
+
+class HashTable:
+    def __init__(self,size):
+        self.values = size * [None]
+
+    def __len__(self):
+        return len(self.values)
+
+    def hash_func(self, val):
+    # It uses repr to obtain a string representation of the object
+    # It removes the "'" to better distribute the results of ord
+    # It uses enumerate and index to improve distribution in similar str
+    # Enumerate starts at 1 so don't multiply by zero the first value    
+        return sum(
+            index * ord(character)
+            for index, character in enumerate(repr(val).lstrip("'"), 1)
+        )
+
+
+    def add(self, val=None):
+
+        if val is None: return
+
+        hash_code = self.hash_func(val)
+        position = hash_code % len(self.values)
+        if self.values[position] is None:
+            self.values[position] = LinkedList(val)
+        else: 
+            self.values[position].add(val)
+
+    def get(self, val=None):
+
+        if val is None: return
+
+        hash_code = self.hash_func(val)
+        position = hash_code % len(self.values)
+        seg =  self.values[position]
+        while seg:
+            if seg.val == val:
+                return seg.val
+            else:
+                seg = seg.next
+        return None
+
---
+++ Chapter01/test_hash_table.py
@@ -0,0 +1,34 @@
+import pytest
+from hash_table import HashTable
+
+def test_should_create_hashtable():
+    assert HashTable(100) is not None
+
+def test_should_report_size():
+    assert len(HashTable(100))==100
+
+def test_should_create_empty_value_slots():
+    # Given
+    expected_values = [None, None, None]
+    hash_table = HashTable(size=3)
+
+    # When
+    actual_values = hash_table.values
+
+    # Then
+    assert actual_values == expected_values
+
+def test_should_insert_values():
+    hash_table = HashTable(size=100)
+
+    hash_table.add("hola")
+    hash_table.add(98.6)
+    hash_table.add(False)
+
+    assert "hola"==hash_table.get("hola")
+    assert 98.6==hash_table.get(98.6)
+    assert False==hash_table.get(False)
+
+@pytest.mark.skip
+def test_should_not_shrink_when_removing_elements():
+    pass
