5d62cb9716ff6b702b6c3ed5ac281567c4a87de8
Federico Scheu <fscheu@gmail.com>
2022-07-31 14:49:24 -0300

    Add Chapter 1, question 4

---
+++ Chapter01/p4_palindrome.py
@@ -0,0 +1,25 @@
+''' p4_palindrome.py '''
+
+# What does it take to be able to write a set of characters the same way forwards and backwards?
+# We need to have an even number of almost all characters,
+# so that half can be on one side and half can be on the other side.
+# At most one character (the middle character) can have an odd count.
+
+from collections import Counter
+import string
+
+
+def is_permutation_palindrome(word) -> bool:
+    ''' check if any permutation of word is a palindrome'''
+    word = clean(word)
+    hash_table = Counter(word)
+    count_odd = 0
+    for a_count in hash_table.values():
+        if a_count % 2:
+            count_odd += 1
+
+    return count_odd <= 1
+
+def clean(word):
+    ''' removes any non ascii chars and converts to lower'''
+    return [c for c in word.lower() if c in string.ascii_lowercase]
---
+++ Chapter01/test_p4_palindrome.py
@@ -0,0 +1,28 @@
+''' test_p4_palindrome.py '''
+
+from p4_palindrome import is_permutation_palindrome
+
+test_cases = [
+    ("aba", True),
+    ("aab", True),
+    ("abba", True),
+    ("aabb", True),
+    ("a-bba", True),
+    ("a-bba!", True),
+    ("Tact Coa", True),
+    ("jhsabckuj ahjsbckj", True),
+    ("Able was I ere I saw Elba", True),
+    ("So patient a nurse to nurse a patient so", False),
+    ("Random Words", False),
+    ("Not a Palindrome", False),
+    ("no x in nixon", True),
+    ("azAZ", True),
+]
+testable_functions = [
+    is_permutation_palindrome,
+]
+
+def test_pal_perm():
+    for f in testable_functions:
+        for [test_string, expected] in test_cases:
+            assert f(test_string) == expected
