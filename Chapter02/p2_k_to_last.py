1bd65aa0eaa67fcb10c0088330f843e2abaf354e
Federico Scheu <fscheu@gmail.com>
2022-08-12 17:41:59 -0300

    Add Ch02, Q02

--- Chapter02/p2_k_to_last.py
+++ Chapter02/p2_k_to_last.py
@@ -1,4 +1,5 @@
 ''' Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list. '''
+from linked_list import LinkedList
 
 def k_to_last(link_list, k):
 
@@ -10,4 +11,18 @@ def k_to_last(link_list, k):
         else:
             n_k = n_k.next
         n_follow = n_follow.next
-    return n_k
+    return n_k.value
+
+test_cases = test_cases = (
+    # list, k, expected
+    ((10, 20, 30, 40, 50), 1, 50),
+    ((10, 20, 30, 40, 50), 5, 10),
+)
+
+def test_k_to_last():
+    for link_l, k, expect in test_cases:
+        l_l = LinkedList(link_l)
+        assert k_to_last(l_l, k) == expect
+
+if __name__ == '__main__':
+    test_k_to_last()
