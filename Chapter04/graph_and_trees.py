b587bec8961405350afdc4c031d8a6a5a36adb36
Federico Scheu <fscheu@gmail.com>
2022-09-07 12:17:42 -0300

    Chapter 04: add BinaryTree str method

--- Chapter04/graph_and_trees.py
+++ Chapter04/graph_and_trees.py
@@ -6,6 +6,19 @@ class BinaryTreeNode:
         self.left = left
         self.right = right
 
+    def disp(self, nesting=0):
+        indent = " " * nesting * 2
+        output = f"{self.value}\n"
+        if self.left is not None:
+            output += f"{indent}L:"
+            output += self.left.disp(nesting + 1)
+        if self.right is not None:
+            output += f"{indent}R:"
+            output += self.right.disp(nesting + 1)
+        return output
+
+    def __str__(self):
+        return self.disp()
 class Node:
     def __init__(self, value=None, neighbors=None) -> None:
         self.value = value
