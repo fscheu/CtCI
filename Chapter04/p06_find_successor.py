96db8eeede64058f048a250adc6df27d968a6276
Federico Scheu <fscheu@gmail.com>
2022-09-28 18:29:22 -0300

    Ch04_P06: fix linter issues

--- Chapter04/p06_find_successor.py
+++ Chapter04/p06_find_successor.py
@@ -1,11 +1,11 @@
-"""Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a
-binary search tree. You may assume that each node has a link to its parent."""
+"""Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node
+in a binary search tree. You may assume that each node has a link to its parent."""
 
 from graph_and_trees import BinaryNode
 
 
-def left_mose_child(node):
-    """ Return the left most child of a given node"""
+def left_most_child(node):
+    """Return the left most child of a given node"""
     next_node = node
     while next_node.left:
         next_node = next_node.left
@@ -16,17 +16,19 @@ def find_successor(node: BinaryNode):
     """return the next node in-order traversal"""
     if node.right:
         # if it has a right child, return the left most node
-        return left_mose_child(node.right)
+        return left_most_child(node.right)
 
     if node.parent:
 
         if node == node.parent.left:
             # if this node was a left child the next node is the parent
             return node.parent
-        else:
-            # if this node was the right child the next node is the next
-            # right-left most sibling
-            next_node = node.parent
-            while next_node.parent and next_node == next_node.parent.right:
-                next_node = next_node.parent
-            return next_node.parent
+
+        # if this node was the right child the next node is the next
+        # right-left most sibling
+        next_node = node.parent
+        while next_node.parent and next_node == next_node.parent.right:
+            next_node = next_node.parent
+        return next_node.parent
+
+    return None
