33f6faf40de1ccf9d78153081582b791a56d3802
Federico Scheu <fscheu@gmail.com>
2022-09-28 20:20:45 -0300

    Ch04_P06: Add test cases

--- Chapter04/p06_find_successor.py
+++ Chapter04/p06_find_successor.py
@@ -1,7 +1,7 @@
 """Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node
 in a binary search tree. You may assume that each node has a link to its parent."""
 
-from graph_and_trees import BinaryNode
+from graph_and_trees import BinaryTreeNode, BinarySearchTree
 
 
 def left_most_child(node):
@@ -12,7 +12,7 @@ def left_most_child(node):
     return next_node
 
 
-def find_successor(node: BinaryNode):
+def find_successor(node: BinaryTreeNode):
     """return the next node in-order traversal"""
     if node.right:
         # if it has a right child, return the left most node
@@ -32,3 +32,29 @@ def find_successor(node: BinaryNode):
         return next_node.parent
 
     return None
+
+def test_in_order_successor():
+    bst = BinarySearchTree()
+    bst.add(20)
+    bst.add(9)
+    bst.add(25)
+    bst.add(5)
+    bst.add(12)
+    bst.add(11)
+    bst.add(14)
+
+    # Test all nodes
+    inputs = [5, 9, 11, 12, 14, 20, 25]
+    outputs = inputs[1:]
+    outputs.append(None)
+
+    for x, y in zip(inputs, outputs):
+        test = bst.get_node(x)
+        succ = find_successor(test)
+        if succ is not None:
+            assert succ.value == y
+        else:
+            assert succ == y
+
+if __name__ == "__main__":
+    test_in_order_successor()            
